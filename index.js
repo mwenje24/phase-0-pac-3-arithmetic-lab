4ff95c17f25544581782df2788f4aaa1d2458b54
Mwenje <mwenje24@gmail.com>
2022-06-02 15:50:06 +0300

    js calculator

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a, b){
+    return a + b
+}
+add(3,4)
+
+function subtract(a, b){
+    return a - b
+}
+subtract(3,4)
+
+function multiply(a, b){
+    return a * b
+}
+multiply(3,4)
+
+function divide(a, b){
+    return a / b
+}
+divide(3,4)
+
+function increment(n){
+    return n += 1;
+}
+increment(991)
+
+function decrement(n){
+    return n -= 1;
+}
+decrement(293)
+
+function makeInt(string){
+    return parseInt(string, 10);
+}
+makeInt("870")
+
+function preserveDecimal(string){
+    return parseFloat(string);
+}
+preserveDecimal("10")
